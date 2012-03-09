f9603cbd3c2dc2b7d2c7abb927c079bc9766e4ea
Kore Nordmann <mail@kore-nordmann.de>
2012-03-09 12:15:50 +0100

    Implemented: View to list hash tags

---
+++ src/app/views/tags/map.js
@@ -0,0 +1,8 @@
+function ( doc ) {
+    if ( doc.type === "tweet" ) {
+        var tags = doc.text.match( /#([A-Za-z0-9_-]+)/g );
+        for ( i in tags ) {
+            emit( tags[i].substr( 1 ).toLowerCase(), null );
+        }
+    }
+}
