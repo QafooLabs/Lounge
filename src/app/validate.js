84c952ab279c2730e178df657af863b847e9b277
Kore Nordmann <mail@kore-nordmann.de>
2012-03-10 09:13:49 +0100

    Updated validation function to validate user documents

--- src/app/validate.js
+++ src/app/validate.js
@@ -1,12 +1,12 @@
 function ( newDoc, oldDoc, userContext ) {
 
-    if ( newDoc.type === "user" ) {
-        // Allow all user documents to be created -- on login for example.
-        return;
-    }
-
     if ( !userContext.name ) {
         throw ( { forbidden: "Please login first." } );
+    }
+
+    if ( ( newDoc.type === "user" ) &&
+         ( newDoc.name === userContext.name ) ) {
+        return;
     }
 
     if ( newDoc.user !== userContext.name ) {
